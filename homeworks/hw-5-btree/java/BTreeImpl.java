b6fc2333beaa8ae11deb2e98047f33994a864241
Kyle Bruin <Kyle.Bruin@me.com>
2013-05-06 13:39:34 -0600

    Java B-Tree

--- homeworks/hw-5-btree/java/BTreeImpl.java
+++ homeworks/hw-5-btree/java/BTreeImpl.java
@@ -1,19 +1,115 @@
 public class BTreeImpl extends BTree {
 
-    public BTreeImpl(int order) {
-	super(order);
-    }
+	public BTreeImpl(int order) {
+		super(order);
+	}
 
-    public void insert(int key) {
+	public void split (Node curNode) {
+		
+	}
+	
+	public void sort(int[] arr) {
+		boolean notDone = true;
+		while (notDone) {
+			notDone = false;
+			for (int i = 0; i<arr.length-1; i++) {
+				if(arr[i] > arr[i+1]) {
+					int temp = arr[i];
+					arr[i]=arr[i+1];
+					arr[i+1] = temp;
+					notDone = true;
+				}
+			}
+		}
+	}
+	
+	public void insert(int key) {
+		// Checks root for key
+		for (int i = 0; i < root.numKeys; i++) {
+			if (root.getKeys()[i] == key) {
+				return;
+			}
+		}
+		// Inserts if root is a leaf
+		if (root.isLeaf) {
+			root.getKeys()[root.numKeys] = key;
+			sort(root.getKeys());
+			root.numKeys++;
+			if (root.numKeys == 5) {
+				split(root);
+			}
+			return;
+		}
+		// Traverses down tree
+		boolean done = false;
+		while (!done) {
+			for (int j = 0; j < root.numKeys; j++) {
+				if (key < root.getKeys()[j]) {
+					done = true;
+					insertKey(root.getChildren()[j], key);
+				}
+			}
+			done = true;
+			insertKey(root.getChildren()[root.numKeys + 1], key);
+		}
+	}
 
-    }
+	public void insertKey(Node curNode, int key) {
+		for (int i = 0; i < curNode.numKeys; i++) {
+			if (root.getKeys()[i] == key) {
+				return;
+			}
+		}
+		if (curNode.isLeaf) {
+			curNode.getKeys()[curNode.numKeys] = key;
+			sort(curNode.getKeys());
+			curNode.numKeys++;
+			if (curNode.numKeys == 5) {
+				split(curNode);
+			}
+			return;
+		}
+		// Traverses down tree
+		boolean done = false;
+		while (!done) {
+			for (int j = 0; j < curNode.numKeys; j++) {
+				if (key < curNode.getKeys()[j]) {
+					done = true;
+					insertKey(curNode.getChildren()[j], key);
+				}
+			}
+			done = true;
+			insertKey(curNode.getChildren()[curNode.numKeys + 1], key);
+		}
+	}
 
-    public void remove(int key) {
+	public void remove(int key) {
+		
+	}
 
-    }
+	public boolean contains(int key) {
+		return checkKey(root, key);
+	}
 
-    public boolean contains(int key) {
-	return false;
-    }
-
+	public boolean checkKey(Node curNode, int key) {
+		for (int i = 0; i < curNode.numKeys; i++) {
+			if (curNode.getKeys()[i] == key) {
+				return true;
+			}
+		}
+		if (!curNode.isLeaf) {
+			boolean done = false;
+			while (!done) {
+				for (int j = 0; j < curNode.numKeys; j++) {
+					if (key < curNode.getKeys()[j]) {
+						done = true;
+						return checkKey(curNode.getChildren()[j], key);
+					}
+				}
+				done = true;
+				return checkKey(curNode.getChildren()[curNode.numKeys], key);
+			}
+		}
+		return false;
+	}
 }
